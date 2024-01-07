82bd32d822471a78cdbf09060a202ecf34df025e
cocomelonc <cocomelonkz@gmail.com>
2024-01-07 18:06:04 +0600

    chapter 11: encrypt/decrypt configuration example

---
+++ chapter11/01-config-crypto/evil.c
@@ -0,0 +1,29 @@
+/*
+ * Malware Development for Ethical Hackers
+ * evil.c
+ * simple DLL for DLL inject to process
+ * author: @cocomelonc
+*/
+
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  nReason, LPVOID lpReserved) {
+  switch (nReason) {
+  case DLL_PROCESS_ATTACH:
+    MessageBox(
+      NULL,
+      "Meow from evil.dll!",
+      "=^..^=",
+      MB_OK
+    );
+    break;
+  case DLL_PROCESS_DETACH:
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETACH:
+    break;
+  }
+  return TRUE;
+}
+
