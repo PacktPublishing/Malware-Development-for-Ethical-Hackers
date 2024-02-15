a8ec8b70711cb44781f858ca60833aa5897edc01
cocomelonc <cocomelonkz@gmail.com>
2024-02-15 14:54:28 +0600

    chapter 14: practical example 1

---
+++ chapter14/04-ttps-used-by-apt/evil.c
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+BOOL APIENTRY DllMain(HMODULE hModule,  DWORD  nReason, LPVOID lpReserved) {
+  switch (nReason) {
+  case DLL_PROCESS_ATTACH:
+    MessageBoxA(NULL, "Meow-meow!", "=^..^=", MB_OK);
+    break;
+  case DLL_PROCESS_DETACH:
+    break;
+  case DLL_THREAD_ATTACH:
+    break;
+  case DLL_THREAD_DETACH:
+    break;
+  }
+  return TRUE;
+}
