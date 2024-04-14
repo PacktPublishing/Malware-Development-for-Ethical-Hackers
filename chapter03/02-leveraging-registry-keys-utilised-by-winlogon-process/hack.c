bac4b11781a78caf5d3b176aa8c0f5c73044661d
cocomelonc <cocomelonkz@gmail.com>
2024-04-14 14:29:13 +0300

    chapter 03: 01, 02, 03, 04 update code

---
+++ chapter03/02-leveraging-registry-keys-utilised-by-winlogon-process/hack.c
@@ -0,0 +1,12 @@
+/*
+ * Malware Development for Ethical Hackers
+ * hack.c
+ * "Hello, Packt" messagebox
+ * author: @cocomelonc
+*/
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+  MessageBoxA(NULL, "Hello, Packt!","=^..^=", MB_OK);
+  return 0;
+}
